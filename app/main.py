43843dee8da20bbb6ed24b7e478631125b3cff7a
jandortiz <jefferson.garciaor@gmail.com>
2025-09-08 20:29:35 +0200

    feat: agrega router relacionado con datos de la EMT.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, Request
-from routers.bicimad_router import router
+from routers.bicimad_router import bicimad_router
+from routers.emt_router import emt_router
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
-app.include_router(router)
+app.include_router(bicimad_router)
+app.include_router(emt_router)
+
 app.mount("/frontend", StaticFiles(directory="../frontend"), name="frontend")
 templates = Jinja2Templates(directory="../frontend")
 
